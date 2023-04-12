1edf958d721635ca5a7b03ba458d134edcd596c1
Harshit Viren Shah <hvshah97@gmail.com>
2023-04-11 18:53:50 -0700

    Added NinjaTraining and CherryPickup2

--- Dynamic Programming/CherryPickup2.py
+++ Dynamic Programming/CherryPickup2.py
@@ -1,61 +1,64 @@
 from typing import List
+
 import numpy as np
 
 
-class NinjaTraining:
-    def trainingRecursion(self, points: List[List[int]], day: int, lastTask: int) -> int:
-        if day == 0:
-            maxPoints = 0
-            for task in range(len(points[0])):
-                if task != lastTask:
-                    maxPoints = max(points[0][task], maxPoints)
-            return maxPoints
+class CherryPickup2:
+    dp = 0
 
-        maxPoints = 0
-        for task in range(len(points[0])):
-            if task != lastTask:
-                maxPoints = max(maxPoints, points[day][task] + self.trainingRecursion(points, day - 1, task))
-        return maxPoints
+    def cherryPickupRecursion(self, grid: List[List[int]], level: int, robot1: int, robot2: int) -> int:
+        # Base Case - Last level
+        if robot1 < 0 or robot1 >= len(grid[0]) or robot2 < 0 or robot2 >= len(grid[0]):
+            return 0
+        if level == len(grid) - 1:
+            if robot1 == robot2:
+                return grid[level][robot1]
+            else:
+                return grid[level][robot1] + grid[level][robot2]
+        maxValue = 0
+        counter = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if robot1 == robot2:
+                    maxValue = max(maxValue,
+                                   self.cherryPickupRecursion(grid, level + 1, robot1 + i, robot2 + j) + grid[level][
+                                       robot1])
+                else:
+                    maxValue = max(maxValue,
+                                   self.cherryPickupRecursion(grid, level + 1, robot1 + i, robot2 + j) + grid[level][
+                                       robot1] + grid[level][robot2])
+        return maxValue
 
-    def trainingDynamicProgrammingMemoization(self, points: List[List[int]], day: int, lastTask: int, memoization: List[List[int]]) -> int:
-        if day == 0:
-            maxPoints = 0
-            for task in range(len(points[0])):
-                if task != lastTask:
-                    maxPoints = max(points[0][task], maxPoints)
-            return maxPoints
+    def cherryPickupMemoization(self, grid: List[List[int]], level: int, robot1: int, robot2: int) -> int:
+        if robot1 < 0 or robot1 >= len(grid[0]) or robot2 < 0 or robot2 >= len(grid[0]):
+            return 0
+        if level == len(grid) - 1:
+            if robot1 == robot2:
+                return grid[level][robot1]
+            else:
+                return grid[level][robot1] + grid[level][robot2]
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if robot1 == robot2:
+                    self.dp[level][robot1][robot2] = max(self.dp[level][robot1][robot2],
+                                                         self.cherryPickupRecursion(grid, level + 1, robot1 + i,
+                                                                                    robot2 + j) + grid[level][robot1])
+                else:
+                    self.dp[level][robot1][robot2] = max(self.dp[level][robot1][robot2],
+                                                         self.cherryPickupRecursion(grid, level + 1, robot1 + i,
+                                                                                    robot2 + j) + grid[level][robot1] +
+                                                         grid[level][robot2])
+        return self.dp[level][robot1][robot2]
 
-        if memoization[day][lastTask] != -1:
-            return memoization[day][lastTask]
+    def cherryPickupRecursionCalling(self, grid: List[List[int]]) -> int:
+        return self.cherryPickupRecursion(grid, 0, 0, len(grid[0]) - 1)
 
-        maxPoints = 0
-        for task in range(len(points[0])):
-            if task != lastTask:
-                maxPoints = max(maxPoints,
-                                points[day][task] + self.trainingDynamicProgrammingMemoization(points, day - 1, task, memoization))
-        memoization[day][lastTask] = maxPoints
-        return memoization[day][lastTask]
-
-    def trainingDynamicProgrammingTabulation(self, points: List[List[int]], day: int, lastTask: int) -> int:
-        tabulation = np.zeros((len(points[0]), len(points)), int).tolist()
-        for i in range(len(points[0])):
-            val = points[0].pop(i)
-            tabulation[0][i] = max(points[0])
-            points[0].insert(i, val)
-
-        for day in range(1, len(points)):
-            for last in range(len(points[0])):
-                for task in range(len(points[0])):
-                    if task != last:
-                        tabulation[day][last] = max(tabulation[day][last],
-                                                    points[day][task] + tabulation[day - 1][task])
-        return tabulation[-1][-1]
-
-    def ninjaTraining(self, n: int, points: List[List[int]]) -> int:
-        memoization = [[-1] * (len(points[0]) + 1)] * len(points)
-        return self.trainingDynamicProgrammingTabulation(points, n - 1, len(points[0]))
+    def cherryPickupMemoizationCalling(self, grid: List[List[int]]) -> int:
+        self.dp = [[[0 for k in range(len(grid))] for j in range(len(grid[0]))] for i in range(len(grid[0]))]
+        return self.cherryPickupMemoization(grid, 0, 0, len(grid[0]) - 1)
 
 
 if __name__ == '__main__':
-    ninjaTraining = NinjaTraining()
-    print(ninjaTraining.ninjaTraining(3, [[1, 2, 5], [3, 1, 1], [3, 3, 3]]))
+    cherryPickup2 = CherryPickup2()
+    print(cherryPickup2.cherryPickupRecursionCalling([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]))
+    print(cherryPickup2.cherryPickupMemoizationCalling([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]))
---
+++ Dynamic Programming/NinjaTraining.py
@@ -0,0 +1,63 @@
+from typing import List
+import numpy as np
+
+
+class NinjaTraining:
+    def trainingRecursion(self, points: List[List[int]], day: int, lastTask: int) -> int:
+        if day == 0:
+            maxPoints = 0
+            for task in range(len(points[0])):
+                if task != lastTask:
+                    maxPoints = max(points[0][task], maxPoints)
+            return maxPoints
+
+        maxPoints = 0
+        for task in range(len(points[0])):
+            if task != lastTask:
+                maxPoints = max(maxPoints, points[day][task] + self.trainingRecursion(points, day - 1, task))
+        return maxPoints
+
+    def trainingDynamicProgrammingMemoization(self, points: List[List[int]], day: int, lastTask: int,
+                                              memoization: List[List[int]]) -> int:
+        if day == 0:
+            maxPoints = 0
+            for task in range(len(points[0])):
+                if task != lastTask:
+                    maxPoints = max(points[0][task], maxPoints)
+            return maxPoints
+
+        if memoization[day][lastTask] != -1:
+            return memoization[day][lastTask]
+
+        maxPoints = 0
+        for task in range(len(points[0])):
+            if task != lastTask:
+                maxPoints = max(maxPoints,
+                                points[day][task] + self.trainingDynamicProgrammingMemoization(points, day - 1, task,
+                                                                                               memoization))
+        memoization[day][lastTask] = maxPoints
+        return memoization[day][lastTask]
+
+    def trainingDynamicProgrammingTabulation(self, points: List[List[int]], day: int, lastTask: int) -> int:
+        tabulation = np.zeros((len(points[0]), len(points)), int).tolist()
+        for i in range(len(points[0])):
+            val = points[0].pop(i)
+            tabulation[0][i] = max(points[0])
+            points[0].insert(i, val)
+
+        for day in range(1, len(points)):
+            for last in range(len(points[0])):
+                for task in range(len(points[0])):
+                    if task != last:
+                        tabulation[day][last] = max(tabulation[day][last],
+                                                    points[day][task] + tabulation[day - 1][task])
+        return tabulation[-1][-1]
+
+    def ninjaTraining(self, n: int, points: List[List[int]]) -> int:
+        memoization = [[-1] * (len(points[0]) + 1)] * len(points)
+        return self.trainingDynamicProgrammingTabulation(points, n - 1, len(points[0]))
+
+
+if __name__ == '__main__':
+    ninjaTraining = NinjaTraining()
+    print(ninjaTraining.ninjaTraining(3, [[1, 2, 5], [3, 1, 1], [3, 3, 3]]))
