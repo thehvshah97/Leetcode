0e2799f819fc7b6b497ed847e09e32a4a0f650af
Harshit Viren Shah <hvshah97@gmail.com>
2023-05-06 13:04:02 -0700

    Added Tabulation Solution

--- Dynamic Programming/CountSubsetsWithSum.py
+++ Dynamic Programming/CountSubsetsWithSum.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class CountSubsets:
     def recursion(self, arr: list, index: int, target: int) -> int:
         if target == 0:
@@ -36,9 +39,24 @@ class CountSubsets:
         dp = [[-1] * (target + 1)] * len(arr)
         return self.dynamicProgramming(arr, len(arr) - 1, target, dp)
 
+    def dynamicProgrammingTabulation(self, arr: list, target: int) -> int:
+        dp = np.zeros([len(arr), target + 1], dtype=int)
+        for i in range(len(arr)):
+            dp[i][0] += 1
+        if arr[0] <= target:
+            dp[0][arr[0]] += 1
+        for i in range(1, len(arr)):
+            for j in range(1, target + 1):
+                not_pick = dp[i - 1][j]
+                pick = 0
+                if arr[i] <= target:
+                    pick = dp[i - 1][j - arr[i]]
+                dp[i][j] = pick + not_pick
+        return dp[-1][-1]
 
 if __name__ == '__main__':
     countSubsets = CountSubsets()
     print(countSubsets.recursionCalling([1, 2, 3, 4], 5))
     print(countSubsets.dynamicProgrammingMemoization([1, 2, 3, 4], 4))
+    print(countSubsets.dynamicProgrammingTabulation([1, 2, 3, 4], 5))
 
