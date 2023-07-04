d6d08e42ccdea542b0516a7fbd401570c297c61a
Harshit Viren Shah <hvshah97@gmail.com>
2023-07-04 13:27:03 -0700

    Added RodCutting Tabulation Optimized

--- Dynamic Programming/RodCutting.py
+++ Dynamic Programming/RodCutting.py
@@ -14,11 +14,25 @@ class RodCutting:
                 if i + 1 <= j:
                     take = dp[i][j - i - 1] + prices[i]
                 dp[i][j] = max(take, not_take)
-        print(dp)
         return dp[-1][-1]
+
+    def dynamicProgrammingTabulationSpace(self, prices: List[int], length: int) -> int:
+        dp = np.zeros(length + 1, dtype=int)
+        for i in range(length + 1):
+            dp[i] = i * prices[0]
+
+        for i in range(1, len(prices)):
+            for j in range(length + 1):
+                not_take = dp[j]
+                take = 0
+                if i + 1 <= j:
+                    take = dp[j - i - 1] + prices[i]
+                dp[j] = max(take, not_take)
+        return dp[-1]
 
 
 if __name__ == '__main__':
     rodCutting = RodCutting()
     print(rodCutting.dynamicProgrammingTabulation([3, 5, 6, 7, 10, 12], 6))
+    print(rodCutting.dynamicProgrammingTabulationSpace([3, 5, 6, 7, 10, 12], 6))
 
